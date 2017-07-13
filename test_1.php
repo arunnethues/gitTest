3c8a3b45b794ab105526128933efe0064acd9a4c
arunnethues <arun@nethues.com>
2017-07-14 04:56:44 +0530

    last commit is updated

--- test_1.php
+++ test_1.php
@@ -3,3 +3,10 @@ hello arun
 it is edited now 12
 how are you
 edit on dev
+commit 5e45bb1077972ae6a0397ab4133771fd0ee61bd2 (HEAD -> newtask, origin/newtask)
+Merge: 269259e eae2037
+Author: arunnethues <arun@nethues.com>
+Date:   Fri Jul 7 23:25:55 2017 +0530
+
+    new updates for new branch
+testing rec
