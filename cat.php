a720c9b603e547c5d81aff6cc821173559fb9331
arunnethues <arun@nethues.com>
2017-07-27 03:07:08 +0530

    new commit

--- cat.php
+++ cat.php
@@ -1,3 +1,11 @@
 new file is created for cat branch
 this file is updated through hooks<br>
 new update live testing
+$ git stash list
+stash@{0}: WIP on master: 686b55d Add wolves.
+stash@{1}: WIP on gerbils: b2bdead Add dogs.
+stash@{2}: WIP on gerbils: b2bdead Add dogs.
+
+$ git stash list
+stash@{0}: WIP on master: 8e5ec59 Merge branch 'testBranch'
+stash@{1}: WIP on master: 8e5ec59 Merge branch 'testBranch'
